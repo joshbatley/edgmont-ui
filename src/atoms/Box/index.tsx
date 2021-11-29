78b48f110d84b08bb770a3f9dc8faa8536973b71
Josh <jbatley12@gmail.com>
2021-11-29 22:00:01 +0000

    basic setup

---
+++ src/atoms/Box/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => {
+  return <button className="bg-green-500">My button</button>
+}
+
+export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.tsx', './src/index.ts'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
