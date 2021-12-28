fa53e96a05076b39430a80093e236cb1a32c3399
Josh <jbatley12@gmail.com>
2021-12-28 16:11:49 +0000

    test fixes

--- src/inputs/Button/BaseButton.tsx
+++ src/inputs/Button/BaseButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Loader } from 'base';
+import { Loader } from 'base/Loader';
 
 export type BaseButtonProps = {
   size?: Size,
