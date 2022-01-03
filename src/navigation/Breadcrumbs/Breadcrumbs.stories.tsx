240bb8ba0f31ddd73395f9a16e056c0be835d289
Josh <jbatley12@gmail.com>
2022-01-03 15:53:51 +0000

    Breadcrumbs

---
+++ src/navigation/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import { Story } from '@storybook/react';
+import { Breadcrumbs } from './';
+import { ChevronRightIcon } from 'base/Icons/Outline';
+
+export default {
+  title: 'Navigation/Breadcrumbs',
+  component: Breadcrumbs,
+  parameters: {
+    backgrounds: {
+      default: 'White',
+    },
+  },
+} as Meta;
+
+export const Default: Story = ({ ...rest }) => (
+  <Breadcrumbs {...rest}>
+    <a href="#">Edgmont</a>
+    <a href="#">Navigation</a>
+    <a href="#">Breadcrumbs</a>
+  </Breadcrumbs >
+);
+
+export const CustomDividers: Story = ({ ...rest }) => (
+  <Breadcrumbs dividerIcon={<ChevronRightIcon />} {...rest}>
+    <a href="#">With</a>
+    <a href="#">A</a>
+    <a href="#">Custom</a>
+  </Breadcrumbs >
+);
+
+export const CollaspsedBreadcrumbs: Story = ({ ...rest }) => (
+  <Breadcrumbs maxItems={4} {...rest}>
+    <a href="#">With hidden</a>
+    <a href="#">now</a>
+    <a href="#">you</a>
+    <a href="#">see</a>
+    <a href="#">me</a>
+    <a href="#">Items</a>
+  </Breadcrumbs >
+);
+
---
+++ src/navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { DotsHorizontalIcon } from 'base/Icons/Outline';
+import classNames from 'classnames';
+import { SecondaryButton } from 'inputs';
+
+export type BreadcrumbsProps = {
+  maxItems?: number;
+  dividerIcon?: React.ReactElement;
+  itemsBefore?: number;
+  itemsAfter?: number;
+};
+
+const Item: React.FC<{ item: any, isLastItem?: boolean, dividerIcon: React.ReactElement }> = ({
+  item, isLastItem = false, dividerIcon,
+}) => (
+  <div className={classNames('inline-flex items-center', !isLastItem && 'text-gray-500')}>
+    {item}
+    {!isLastItem && React.cloneElement(dividerIcon, {
+      width: 16, height: 16, className: 'inline-block mx-1.5',
+    })}
+  </div>
+);
+
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
+  children, maxItems = 8, itemsBefore = 1, itemsAfter = 1, dividerIcon = <div>/</div>,
+}) => {
+  let [showAll, setShow] = useState(false);
+  let items = React.Children.toArray(children);
+
+  if (items.length <= maxItems) {
+    return (
+      <div>
+        {items.map((c, idx) => (
+          <Item key={idx} item={c} isLastItem={idx === items.length - 1} dividerIcon={dividerIcon} />
+        ))}
+      </div>
+    );
+  }
+
+  let beforeItems = items.slice(0, itemsBefore);
+  let middleItems = items.slice(itemsBefore, -itemsAfter);
+  let afterItems = items.slice(-itemsAfter);
+
+  return (
+    <div className="flex items-center">
+      {beforeItems.map((c, idx) => (
+        <Item key={idx} item={c} dividerIcon={dividerIcon} />
+      ))}
+      {showAll ? middleItems.map((c, idx) => (
+        <Item key={idx} item={c} dividerIcon={dividerIcon} />
+      )) : (
+        <div className='inline-flex items-center'>
+          <SecondaryButton size='small' onClick={() => setShow(true)}>
+            <DotsHorizontalIcon height={16} width={16} />
+          </SecondaryButton>
+          {React.cloneElement(dividerIcon, {
+            width: 16, height: 16, className: 'inline-block text-gray-500 mx-1.5',
+          })}
+        </div>
+      )}
+      {afterItems.map((c, idx) => (
+        <Item key={idx} item={c} isLastItem={idx === afterItems.length - 1} dividerIcon={dividerIcon} />
+      ))}
+    </div >
+  );
+
+
+};
