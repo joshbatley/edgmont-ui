fd59fe7b73e24e9ffc5b8eafb317d0fb6d41884f
Matthew Chambers <matster246@gmail.com>
2022-02-25 22:22:19 +0000

    adds classname property to label component (#66)

--- src/inputs/WithLabel/Label.tsx
+++ src/inputs/WithLabel/Label.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Text } from 'data';
 
 export type LabelProps = {
   value: React.ReactNode;
+  className?: string;
   children: React.ReactElement;
 };
 
-export const Label: React.FC<LabelProps> = ({ value, children }) => {
+export const Label: React.FC<LabelProps> = ({ value, className, children }) => {
   let isRequired = children.props.required;
   return (
-    <label className="block">
+    <label className={clsx('block', className)}>
       <div className="flex text-sm mb-1 ml-1">
         {value} {isRequired && <Text className="text-red-500">*</Text>}
       </div>
