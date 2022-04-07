3b5fbbfe16bc82bd79098f6aa0ddbefad0e87bf7
Josh Batley <jbatley12@gmail.com>
2022-04-07 21:33:15 +0100

    Move to Monorepo (#69)

---
+++ scripts/move-docs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+files=()
+for d in packages/*; do
+  files+=("${d//packages\//}")
+  mkdir docs/"${d//packages/}"
+  rsync -a "$d"/build/ "${d//packages/docs}"
+done
+
+links=""
+for f in "${files[@]}"; do
+  links+="<a href='$f'>$f<\/a>\n"
+done
+
+sed "s/{{links}}/${links}/g" scripts/doc-template.html > docs/index.html
