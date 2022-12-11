1ecb5fb7bc9da1c0bfa175d23ded04b76f94c211
Josh <jbatley12@gmail.com>
2022-12-11 21:22:18 +0000

    updated themes

---
+++ packages/react/src/utils/Theme/Breakpoints.ts
@@ -0,0 +1,15 @@
+export const breakpoints = ['640px', '768px', '1024px', '1280px'];
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* @ts-ignore enable */
+breakpoints.sm = breakpoints[0];
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* @ts-ignore enable */
+breakpoints.md = breakpoints[1];
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* @ts-ignore enable */
+breakpoints.lg = breakpoints[2];
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* @ts-ignore enable */
+breakpoints.xl = breakpoints[3];
+
