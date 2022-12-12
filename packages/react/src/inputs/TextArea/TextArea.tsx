a59ae53e826df7ae4905f179b36d832fb73c9734
Josh <jbatley12@gmail.com>
2022-12-12 08:49:13 +0000

    migrat textarea

--- packages/react/src/inputs/TextArea/TextArea.tsx
+++ packages/react/src/inputs/TextArea/TextArea.tsx
@@ -1,6 +1,17 @@
-import React, { forwardRef } from 'react';
-import clsx from 'clsx';
+import styled from 'styled-components';
 
-export const TextArea = forwardRef<HTMLTextAreaElement, React.ComponentPropsWithoutRef<'textarea'>>(({ className, ...rest }, ref) => (
-  <textarea className={clsx('shadow-sm mt-1 p-2 block h-24 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:z-10', className)} ref={ref} {...rest} />
-));
+export const TextArea = styled.textarea`
+  box-shadow: ${({ theme }) => theme.shadows[1]};
+  margin-top: ${({ theme }) => theme.space[1]};
+  padding: ${({ theme }) => theme.space[2]};
+  width: 100%;
+  height: ${({ theme }) => theme.sizes[16]};
+  border: ${({ theme }) => theme.borders.gray[1]};
+  border-radius: ${({ theme }) => theme.radii[2]};
+  :focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+    box-shadow: ${({ theme }) => theme.shadows.focus};
+    z-index: 10;
+  }
+`;
