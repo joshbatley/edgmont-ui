35108f14c3ea52f5685ae132834564d7fafc59e3
Josh <jbatley12@gmail.com>
2022-12-24 18:13:55 +0000

    fixed theme

---
+++ packages/react/src/utils/Theme/spaces.ts
@@ -0,0 +1,25 @@
+export const spaces = [
+  0,
+  '0.25rem',
+  '0.5rem',
+  '0.75rem',
+  '1rem',
+  '1.25rem',
+  '1.5rem',
+  '1.75rem',
+  '2rem',
+  '2.25rem',
+  '2.5rem',
+];
+
+export const spaceYBetween = spaces.map((s) => (
+  `> :not(:first-child) {
+      margin-top: ${s};
+    }`));
+
+export const spaceXBetween = spaces.map((s) => (
+  `> :not(:last-child) {
+      margin-right: calc(${s}/2);
+    }`));
+
+
