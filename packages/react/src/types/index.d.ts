96533bf89bac60ee6a0ba231a0cc86ae87addf4f
Josh <jbatley12@gmail.com>
2022-12-24 18:48:41 +0000

    removed css

--- packages/react/src/types/index.d.ts
+++ packages/react/src/types/index.d.ts
@@ -1,8 +1,3 @@
-declare module '*.css' {
-  const styles: { [className: string]: string };
-  export default styles;
-}
-
 type OverriderDefaults = Record<Partial<Property>, string>;
 
 type Colors = 'primary' | 'success' | 'info' | 'error' | 'warning' | 'dark';
