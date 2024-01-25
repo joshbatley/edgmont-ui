b9f5cfb948f3baffd71994a93f25a7ba55f26953
Josh <jbatley12@gmail.com>
2024-01-25 17:43:56 +0000

    test

--- packages/react/src/shared/isObjectAndValid.ts
+++ packages/react/src/shared/isObjectAndValid.ts
@@ -1,3 +1,4 @@
+import { isValidElement } from 'react';
 import { ObjectKey, WithKey } from '../types';
 
 export function isObjectAndValid<T extends ObjectKey>(icon: any, key: T): icon is WithKey<T> {
@@ -5,5 +6,5 @@ export function isObjectAndValid<T extends ObjectKey>(icon: any, key: T): icon i
     && icon !== null
     && typeof icon === 'object'
     && key in icon
-    && React.isValidElement(icon[key]);
+    && isValidElement(icon[key]);
 }
