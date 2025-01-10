fb009831f62c13b0fbd95b39925b8db2131a2014
Josh <jbatley12@gmail.com>
2025-01-10 14:02:47 +0000

    added color props

--- packages/react/src/data/Cards/Card.tsx
+++ packages/react/src/data/Cards/Card.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
-import { FlexboxProps, LayoutProps, SpaceProps, flexbox, layout, space } from 'styled-system';
+import { FlexboxProps, LayoutProps, SpaceProps, flexbox, layout, space, ColorProps, color } from 'styled-system';
 
 export type CardProps = {
   children: React.ReactNode;
   style?: React.CSSProperties;
-} & LayoutProps & FlexboxProps & SpaceProps;
+} & LayoutProps & FlexboxProps & SpaceProps & ColorProps;
 
 export const Card = styled.div<CardProps>`
   ${layout}
   ${flexbox}
   ${space}
+  ${color}
   border-radius: ${({ theme }) => theme.radii[3]};
   background: ${({ theme }) => theme.colors.background};
   box-shadow: ${({ theme }) => theme.shadows.base[0]};
